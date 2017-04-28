91024da15abe306aaad79233f0dc111080d74c47
Sanchit Sharma <sanc.sharma3@gmail.com>
2017-04-28 05:44:16 -0400

    Update dbConfig.js

--- routes/dbConfig.js
+++ routes/dbConfig.js
@@ -2,7 +2,7 @@
  * Created by Abishek on 3/30/2017.
  */
 module.exports={
-    user:"sasharma",
-    password:"Oracle$1",
-    connectionstring:"oracle.cise.ufl.edu:1521/orcl"
+    user:"",
+    password:"",
+    connectionstring:""
 };
